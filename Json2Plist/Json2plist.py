40177a0d986379c61c11a14aee325f9c5410b298
wshxbqq <wshxbqq@gmail.com>
2014-04-03 09:47:23 +0800

    upload
    upload

---
+++ Json2Plist/Json2plist.py
@@ -0,0 +1,29 @@
+import sublime, sublime_plugin
+import os
+import subprocess
+import thread
+import sys
+default_encoding = 'utf-8'
+if sys.getdefaultencoding() != default_encoding:
+    reload(sys)
+    sys.setdefaultencoding(default_encoding)
+
+
+
+PLUGIN_DIRECTORY = os.getcwd().replace(os.path.normpath(os.path.join(os.getcwd(), '..', '..')) + os.path.sep, '').replace(os.path.sep, '/')
+PLUGIN_PATH = os.getcwd().replace(os.path.join(os.getcwd(), '..', '..') + os.path.sep, '').replace(os.path.sep, '/')
+
+class ConvertJsonCommand(sublime_plugin.WindowCommand):
+	def run(self):
+		path=self.window.active_view().file_name()
+		jsPath=os.path.join(PLUGIN_PATH,"converter.js")
+		cmdStr="node "+"\""+jsPath+"\""+" \""+path+"\""
+		proce = subprocess.Popen(cmdStr ,shell=True,stdout=subprocess.PIPE)
+		output=proce.communicate()[0]
+		print cmdStr
+		newView=self.window.new_file()
+		newView.set_syntax_file("Packages/XML/XML.tmLanguage")
+		edit = newView.begin_edit()
+		newView.insert(edit, newView.size(), output)
+		newView.end_edit(edit)
+		pass
---
+++ README.md
@@ -0,0 +1,8 @@
+# Easy Convert Json To Plist
+
+ 
+
+## Install
+	1.Install [NODEJS](http://www.nodejs.org/)
+	2.Control + Shift + p
+	3.input Convert 2 Plist
